0c0a6da48003a8b95456a1d241dbc651e5c6c2e8
omkar-borhade <omkarborhade321952@gmail.com>
2024-07-16 00:19:30 +0530

    data base connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)
+.then(()=>
+{console.log("database connected!!")})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express()
-
 const port = 3000
+
 // app.use(cors({
 //     credentials:true,
 //     origin:'http://localhost:5173'
