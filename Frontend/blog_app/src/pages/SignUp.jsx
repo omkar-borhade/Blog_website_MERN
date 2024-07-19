a61215fe803c4b8ac18b0de546d3a2e1b973b6a5
omkar-borhade <omkarborhade321952@gmail.com>
2024-07-20 01:09:28 +0530

    add header component with user data

--- Frontend/blog_app/src/pages/SignUp.jsx
+++ Frontend/blog_app/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import{ useState } from 'react'
 import {Link,useNavigate} from 'react-router-dom'
 import{Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
 import axios from 'axios'
+import OAuth from '../components/OAuth'
 export default function SignUp() {
   const[formData,setFormData]=useState({});
   const [errorMessage,setErrorMessage]=useState(null);
@@ -102,6 +103,7 @@ export default function SignUp() {
           ):'Sign UP'
         }
         </Button>
+        <OAuth/>
         </form>
         <div className="flex gap-2 text-sm mt-5">
           <span>Have an account?</span>
@@ -109,6 +111,7 @@ export default function SignUp() {
           Sign In
           </Link>
         </div>
+       
         {
           errorMessage && (
             <Alert className='mt-5' color='failure'>
