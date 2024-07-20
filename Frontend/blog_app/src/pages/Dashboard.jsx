5da987a125aa0b157b48832c7d6c3ba847e953ef
omkar-borhade <omkarborhade321952@gmail.com>
2024-07-20 13:19:23 +0530

    complete sidebar of the dashbordd

--- Frontend/blog_app/src/pages/Dashboard.jsx
+++ Frontend/blog_app/src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect,useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 function Dashboard() {
+  const location =useLocation();
+  const[tab,setTab]=useState('');
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search)
+    const tabFormUrl=urlParams.get('tab');
+    if(tabFormUrl){
+      setTab(tabFormUrl);
+    }
+
+  },[location.search])
   return (
-    <div>Dashboard</div>
+  <div className='min-h-screen flex flex-col md:flex-row'>
+       <div className='md:w-56'>
+
+      <DashSidebar/>
+
+    </div>
+      {tab === 'profile' && <DashProfile/>}
+  </div>
+   
+  
   )
 }
 
